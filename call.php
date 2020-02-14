8369973953f99d04f462618e756ff1c5ce1be398
zhorton <zak.horton@vml.com>
2020-02-13 23:59:47 -0600

    Php magic call method tutorial

--- call.php
+++ call.php
@@ -1,4 +1,18 @@
 <?php
 
-//
+class Building
+{
+    private function exploded($becauseYou)
+    {
+        echo "The Building exploded because you {$becauseYou}. Shouldn't of done that.";
+    }
 
+    public function __call($method, $parameters)
+    {
+        call_user_func_array([$this, $method], $parameters);
+    }
+}
+
+$tower = new Building;
+
+$tower->exploded("pressed the red button");
