87ae5f1a9abc6922cb3f4c07f1bf70d372d2fc2f
zhorton <zak.horton@vml.com>
2020-04-09 05:41:47 -0500

    Added isset.php Tutorial Content

--- isset.php
+++ isset.php
@@ -1,3 +1,29 @@
 <?php
 
-//
+class Channel
+{
+    public $name;
+
+    public function __isset($property)
+    {
+        $exists = isset($this->$property);
+        $not_empty = !empty($this->$property);
+
+        return (bool) $exists and $not_empty;
+    }
+
+    public function __get($property)
+    {
+        return isset($this->$property) ? true : false;
+    }
+}
+
+$channel = new Channel;
+
+$channel->name = "Clean Code Studio (Please Like)";
+
+if ($channel->name) {
+    echo "Channel Name: {$channel->name}";
+} else {
+    echo "Channel Name Does Not Exist";
+}
